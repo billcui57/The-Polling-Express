00fbcd16b6ce7fbb8ffad50f9b40a889029557d2
Bill Cui <b22cui@uwaterloo.ca>
2022-05-29 17:27:07 -0400

    RPS user

--- src/user.c
+++ src/user.c
@@ -19,13 +19,17 @@ void task1() {
 
   printf(&pc, "1: I am in game %d\r\n", game_id);
 
-  for (;;) {
-    int result = Play(game_id, MOVE_PAPER);
+  for (int i = 0;; i++) {
+
+    int move = (i % 3) + 2;
+    printf(&pc, "1: Move: %d\r\n", move);
+
+    int result = Play(game_id, move);
 
     if (result == RESPONSE_YOU_LOST) {
       printf(&pc, "1: I lost\r\n");
     } else if (result == RESPONSE_TIE) {
-      printf(&pc, "1: I tied r\n");
+      printf(&pc, "1: I tied \r\n");
     } else if (result == RESPONSE_YOU_WON) {
       printf(&pc, "1: I won \r\n");
     } else if (result == RESPONSE_NOT_YOUR_GAME) {
@@ -38,21 +42,23 @@ void task1() {
     }
   }
 
-  Create(-10, task2);
-  Create(-10, task1);
+  // Create(-10, task2); <- uncomment for stress test
+  // Create(-10, task1);
 }
 
 void task2() {
   int game_id;
   int status = SignUp(&game_id);
 
-  for (unsigned int i = 0; i < 100; i++) {
-    int result = Play(game_id, MOVE_ROCK);
+  for (int i = 0; i < 100; i++) {
+    int move = ((i % 7) % 3) + 2;
+    printf(&pc, "2: Move: %d\r\n", move);
+    int result = Play(game_id, move);
 
     if (result == RESPONSE_YOU_LOST) {
       printf(&pc, "2: I lost\r\n");
     } else if (result == RESPONSE_TIE) {
-      printf(&pc, "2: I tied r\n");
+      printf(&pc, "2: I tied \r\n");
     } else if (result == RESPONSE_YOU_WON) {
       printf(&pc, "2: I won \r\n");
     } else if (result == RESPONSE_NOT_YOUR_GAME) {
@@ -72,62 +78,9 @@ void task2() {
     printf(&pc, "2: Successfully quit game \r\n");
   }
 
-  Create(-10, task1);
-  Create(-10, task2);
+  // Create(-10, task1);
+  // Create(-10, task2);
 }
-
-// void task1() {
-//   int game_id;
-//   int status = SignUp(&game_id);
-
-//   if (status == RESPONSE_NO_FREE_GAMES) {
-//     printf(&pc, "1: All games full \r\n");
-//     Exit();
-//   }
-//   int result = Play(game_id, MOVE_PAPER);
-
-//   if (result == RESPONSE_YOU_LOST) {
-//     printf(&pc, "1: I lost\r\n");
-//   } else if (result == RESPONSE_TIE) {
-//     printf(&pc, "1: I tied r\n");
-//   } else if (result == RESPONSE_YOU_WON) {
-//     printf(&pc, "1: I won \r\n");
-//   } else if (result == RESPONSE_NOT_YOUR_GAME) {
-//     printf(&pc, "1: Not my game \r\n");
-//   } else {
-//     printf(&pc, "1: Uh oh \r\n");
-//   }
-
-//   Create(-10, task2);
-// }
-
-// void task2() {
-//   int game_id;
-//   int status = SignUp(&game_id);
-
-//   if (status == RESPONSE_NO_FREE_GAMES) {
-//     printf(&pc, "2: All games full \r\n");
-//     Exit();
-//   }
-
-//   printf(&pc, "2: I am in game %d\r\n", game_id);
-
-//   int result = Play(game_id, MOVE_ROCK);
-
-//   if (result == RESPONSE_YOU_LOST) {
-//     printf(&pc, "2: I lost\r\n");
-//   } else if (result == RESPONSE_TIE) {
-//     printf(&pc, "2: I tied r\n");
-//   } else if (result == RESPONSE_YOU_WON) {
-//     printf(&pc, "2: I won \r\n");
-//   } else if (result == RESPONSE_NOT_YOUR_GAME) {
-//     printf(&pc, "2: Not my game \r\n");
-//   } else {
-//     printf(&pc, "2: Uh oh \r\n");
-//   }
-
-//   Create(-10, task1);
-// }
 
 void task_k2perf() {
   int sz[] = {4, 64, 256};
