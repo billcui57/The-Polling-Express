ecd9e48ad7424a2cffacbabdfec43cbc9ff9332a
Bill Cui <billcui@v1040-wn-rt-a-123-69.campus-dynamic.uwaterloo.ca>
2022-07-09 15:47:54 -0400

    changed track selection UI

--- src/user/user.c
+++ src/user/user.c
@@ -19,29 +19,33 @@ char status[] = "-----";
 // sensor C13
 
 void task_k4_init() {
-
   clear_screen(BW_COM2);
-  printf(BW_COM2, "Which track am I on (a,b)?\r\n");
-  char c = bw_uart_get_char(COM2);
-  which_track = c;
+  while (true) {
 
-  if (c == 'a') {
-    init_tracka(track);
-    mark_switch_broken(track, track_name_to_num(track, "BR156"), DIR_CURVED);
-    mark_switch_broken(track, track_name_to_num(track, "BR155"), DIR_STRAIGHT);
-    mark_switch_broken(track, track_name_to_num(track, "BR16"), DIR_CURVED);
-    mark_switch_broken(track, track_name_to_num(track, "BR3"), DIR_CURVED);
-    mark_sensor_broken(track, track_name_to_num(track, "D1"));
-  } else if (c == 'b') {
-    init_trackb(track);
-    mark_switch_broken(track, track_name_to_num(track, "BR156"), DIR_STRAIGHT);
-    mark_switch_broken(track, track_name_to_num(track, "BR155"), DIR_CURVED);
-    mark_switch_broken(track, track_name_to_num(track, "BR5"), DIR_CURVED);
-    mark_sensor_broken(track, track_name_to_num(track, "C13"));
-  } else {
-    KASSERT(c == 'a' || c == 'b', "Track must be a or b");
-    for (;;)
-      ;
+    printf(BW_COM2, "Which track am I on (a,b)?\r\n");
+    char c = bw_uart_get_char(COM2);
+    which_track = c;
+
+    if (c == 'a') {
+      init_tracka(track);
+      mark_switch_broken(track, track_name_to_num(track, "BR156"), DIR_CURVED);
+      mark_switch_broken(track, track_name_to_num(track, "BR155"),
+                         DIR_STRAIGHT);
+      mark_switch_broken(track, track_name_to_num(track, "BR16"), DIR_CURVED);
+      mark_switch_broken(track, track_name_to_num(track, "BR3"), DIR_CURVED);
+      mark_sensor_broken(track, track_name_to_num(track, "D1"));
+      break;
+    } else if (c == 'b') {
+      init_trackb(track);
+      mark_switch_broken(track, track_name_to_num(track, "BR156"),
+                         DIR_STRAIGHT);
+      mark_switch_broken(track, track_name_to_num(track, "BR155"), DIR_CURVED);
+      mark_switch_broken(track, track_name_to_num(track, "BR5"), DIR_CURVED);
+      mark_sensor_broken(track, track_name_to_num(track, "C13"));
+      break;
+    } else {
+      printf(BW_COM2, "Please enter a valid track\r\n");
+    }
   }
 
   Create(20, nameserver);
